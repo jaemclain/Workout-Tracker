f640af876be315ad10448d36a255cbf5534dd279
Jae McLain <jae.mclain@gmail.com>
2020-11-23 16:15:55 -0800

    updating index.html

--- models/workout.js
+++ models/workout.js
@@ -2,31 +2,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    exerciseType:
-    {
-        type: String,
-        required: true
-    },
-    name:
-    {
-        type: String,
-        required: true
-    },
-    sets:
-    {
-        type: Number,
-        required: true
-    },
-    reps:
-    {
-        type: Number,
-        required: true
-    },
-    duration:
-    {
-        type: Number,
-        required: true
-    }
+    exercises: [{
+        exerciseType:
+        {
+            type: String,
+            required: true
+        },
+        name:
+        {
+            type: String,
+            required: true
+        },
+        sets:
+        {
+            type: Number,
+            required: true
+        },
+        reps:
+        {
+            type: Number,
+            required: true
+        },
+        duration:
+        {
+            type: Number,
+            required: true
+        }
+    }]
 })
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- public/index.js
+++ public/index.js
@@ -26,3 +26,48 @@ $("#make-new").on("click", function (event) {
         });
 });
 
+
+// UPDATE a previous workout
+$.get("/api/getWorkout")
+    .then(function (response) {
+        console.log(response)
+        if (response.length != 0) {
+            var previousworkout = response[response.length - 1]
+            $("#updateexcercise").attr("workoutid", previousworkout._id)
+            for (let i = 0; i < previousworkout.excercises.length; i++) {
+                $("#previousworkout").append(`
+            <tr>
+            <td>${previousworkout.excercises[i].excerciseType}</td>
+            <td>${previousworkout.excercises[i].name}</td>
+            <td>${previousworkout.excercises[i].sets}</td>
+            <td>${previousworkout.excercises[i].reps}</td>
+            <td>${previousworkout.excercises[i].duration}</td>
+            `)
+            }
+        } else {
+            $("#updateexcercise").hide()
+        }
+    });
+
+
+$("#updateexcercise").on("click", function (event) {
+    event.preventDefault()
+    var newWorkout = {
+        excercisetype: $("#excercisetype").val(),
+        name: $("#name").val(),
+        sets: $("#sets").val(),
+        reps: $("#reps").val(),
+        duration: $("#duration").val(),
+    }
+    let id = $("#updateexcercise").attr("workoutid")
+    console.log(newWorkout, id);
+    $.ajax("/api/updateworkout/" + id,
+        {
+            method: "PUT",
+            data: newWorkout
+        })
+        .then(function (response) {
+            console.log(response)
+            location.reload()
+        })
+})
